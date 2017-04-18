e181e6e53e7adbffb3dd6679d830d49a09b7e86b
K-Edward <hwan0092@naver.com>
2017-04-18 13:30:02 +0900

    Add files via upload

--- src/ds_save_mongo_5.js
+++ src/ds_save_mongo_5.js
@@ -1,9 +1,7 @@
+use myDB #DB생성 (이미 존재하면 그대로 사용)
+#아래에서 db는 지금 사용중인 myDB를 나타냄
+db.myCol.insert({"Persons" : {"id":"405", "이름":"js1"}, {"id":"406", "이름":"js2"}})
+#DB에 값 저장함
 
-import json
-
-doc = {"Persons" : [{"id":"405", "이름":"js1"},{"id":"406", "이름":"js2"},{"id":"301", "이름":"js3"},{"id":"102", "이름":"js4"}]}
-
-use examDB
-#db.createCollection("personCol")
-db.personColl.insert(doc)
-db.personColl.find({"Persons.id" : "405"})
+#찾아주는 과정
+db.myCol.find({"Persons.이름":"js2"})
